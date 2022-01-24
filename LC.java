66d84aac024830fef0c1a9fe99d7e0dc465f9be0
Koufuchi <jackkao8752@gmail.com>
2022-01-24 17:08:51 +0800

    update

--- LC.java
+++ LC.java
@@ -237,9 +237,29 @@ public class LC {
         }
     }
 
-    // no12 [48. Rotate Image] Medium
+    // no12 [48. Rotate Image] Medium [Array] [Math] [Matrix]
+    /**
+     * matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+     * except = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+     * 
+     * You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+     * DO NOT allocate another 2D matrix and do the rotation.
+     * 
+     */
+    public void rotate(int[][] matrix) {
+        
+    }
 
-    // no13 [49. Group Anagrams] Medium
+    // no13 [49. Group Anagrams] Medium [String] [Sorting] [HashTable]
+    /**
+     * 
+     * nums = ["eat","tea","tan","ate","nat","bat"]
+     * except = [["bat"],["nat","tan"],["ate","eat","tea"]]
+     */
+    /*static List<List<String>> groupAnagrams(String[] strs) {
+
+        return ans;
+    }*/
 
     // no14 [53. Maximum Subarray] Easy
     /**
@@ -680,6 +700,24 @@ public class LC {
         }
         return (max*(max+1)/2)-cnt;
     }
+    // [變形題] [no55 isAnagram]
+    public boolean isAnagram_extra(String[] s1, String[] s2){
+        // [[bad],[app],[no]] s1
+        // [[on],[pap],[adb]] s2 return true
+        // [[o],[npap],[adb]] s2 return false
+        if(s1.length != s2.length) return false;
+        for(int i=0; i<s1.length; i++){
+            char[] t1 = s1[i].toCharArray();
+            char[] t2 = s2[i].toCharArray();
+            Arrays.sort(t1);
+            Arrays.sort(t2);
+            s1[i] = new String(t1); // String.valueOf(t1);
+            s2[i] = new String(t2);
+        }
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        return Arrays.equals(s1, s2);
+    }
 
 
     // 斷行
@@ -893,6 +931,13 @@ public class LC {
         rs = now();
         int[] no59 = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19}; 
         System.out.println("no59 缺少的數字為: " + object.missingNumber(no59));
+        runtime(rs);
+
+        // no55 變形
+        rs = now();
+        String[] s1 = {"bad", "app", "no", "just", "thecountryside"};
+        String[] s2 = {"on", "pap", "adb", "ustj", "nocitydusthere"};
+        System.out.println("extra 兩陣列中是否皆為可對應變位字: " + object.isAnagram_extra(s1, s2));
         runtime(rs);
     }
 }
